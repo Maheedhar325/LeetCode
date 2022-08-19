b9463d3a25df91354ee8b5739f1d285ac22b96e0
Maheedhar Vundela <53821760+Maheedhar325@users.noreply.github.com>
2022-08-19 15:55:28 +0530

    Time: 1245 ms (11.67%), Space: 13.8 MB (54.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,11 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        q,w = 0,1
-        i = 0
-        while i < n:
-            z = q+w
-            q = w
-            w = z
-            i+=1
-        return q
+        # q,w = 0,1
+        # i = 0
+        # while i < n:
+        #     z = q+w
+        #     q = w
+        #     w = z
+        #     i+=1
+        # return q
+        
+        if n<2:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
         
