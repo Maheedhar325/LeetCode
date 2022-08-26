cf6fe6634a559b2c6879b9111f34182ed25f5d84
Maheedhar Vundela <53821760+Maheedhar325@users.noreply.github.com>
2022-08-26 21:51:17 +0530

    Time: 115 ms (49.61%), Space: 15.1 MB (64.41%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping={}
+        mapping = {}
         for i,n in enumerate(nums):
             diff = target - n
             if diff in mapping:
                 return [mapping[diff],i]
             else:
                 mapping[n] = i
+#         nums.sort()
+#         for i in range(len(nums)):
+#             l = i
+#             r = len(nums) - 1
+#             while l<r:
+#                 total = nums[l] + nums[r]
+#                 if total == target:
+#                     return [l,r]
+#                 elif total > target:
+#                     r -= 1
+#                 else:
+#                     l+=1
+                    
+                    
             
                 
                 
