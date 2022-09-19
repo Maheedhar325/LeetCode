d18d8c0fede1fe63681020edd6002cd4c5b35db8
Maheedhar Vundela <53821760+Maheedhar325@users.noreply.github.com>
2022-09-20 02:14:20 +0530

    Time: 1486 ms (16.69%), Space: 33 MB (9.48%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -7,4 +7,10 @@ class Solution:
             else:
                 return nums[i]
         
+        # add = []
+        # for i in range(len(nums)):
+        #     if nums[i] not in add:
+        #         add.append(nums[i])
+        #     else:
+        #         return nums[i]        
         
