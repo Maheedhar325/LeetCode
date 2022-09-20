a7d14b49e594df688f79972ae2919e4bc4886d8a
Maheedhar Vundela <53821760+Maheedhar325@users.noreply.github.com>
2022-09-20 20:10:01 +0530

    Time: 340 ms (21.59%), Space: 15.6 MB (13.03%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(len(nums) + 1):
-            if count in nums:
-                count = count + 1
-                continue
-            else:
-                return count
+        # count = 0
+        # for i in range(len(nums) + 1):
+        #     if count in nums:
+        #         count = count + 1
+        #         continue
+        #     else:
+        #         return count
+        
+        c = Counter(nums)
+        n = len(nums)
+        for i in range(n+1):
+            if i not in c:
+                return i
             
         
